83363489e06369ceadd140c69d5e2c3cbfe2f530
Tilo Himmelsbach <tilo.himmelsbach@iais.fraunhofer.de>
2022-01-26 17:26:30 +0100

    plotly_dash_dataclassed.py

--- plotly_dash_utils/plotly_dash_dataclassed.py
+++ plotly_dash_utils/plotly_dash_dataclassed.py
@@ -5,12 +5,42 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from beartype.roar import BeartypeCallHintPepParamException
+from dash import Dash
 from dash.dependencies import Input, Output, State
 
 from misc_utils.dataclass_utils import serialize_dataclass, deserialize_dataclass
 from misc_utils.utils import just_try
 
-app = dash.Dash(__name__)
+
+class DashDataclasses(Dash):
+    def callback_dataclassed(self, *_args, **_kwargs):
+        """
+        wraps the dash-decorator and putting another decorator in front of it
+        1. serialize_deserialize_dataclasses_wrapper
+        2. app.callback, super().callback here
+        :return:
+        """
+
+        def some_decorator(fun):
+            def serialize_deserialize_dataclasses_wrapper(*args, **kwargs):
+                o = fun(
+                    *[deserialize(s) for s in args],
+                    **{k: deserialize(s) for k, s in kwargs.items()},
+                )
+                if isinstance(o, (list, tuple)):
+                    o = [serialize_dataclass(dc) for dc in o]
+                else:
+                    o = serialize_dataclass(o)
+                return o
+
+            return self.callback(*_args, **_kwargs)(
+                serialize_deserialize_dataclasses_wrapper
+            )
+
+        return some_decorator
+
+
+app = DashDataclasses(__name__)
 
 app.layout = html.Div(
     [
@@ -47,27 +77,7 @@ def deserialize(x: Any):
         return x
 
 
-def app_callback_dataclassed(*args, **kwargs):
-    def some_decorator(fun):
-        def inner_function(*_args, **_kwargs):
-            # assert all((isinstance(s,str) for s in _args))
-            # assert all((isinstance(s,str) for s in _kwargs.values()))
-            o = fun(
-                *[deserialize(s) for s in _args],
-                **{k: deserialize(s) for k, s in _kwargs.items()},
-            )
-            if isinstance(o, (list, tuple)):
-                o = [serialize_dataclass(dc) for dc in o]
-            else:
-                o = serialize_dataclass(o)
-            return o
-
-        return app.callback(*args, **kwargs)(inner_function)
-
-    return some_decorator
-
-
-@app_callback_dataclassed(
+@app.callback_dataclassed(
     Output(component_id="my-output", component_property="children"),
     Output("my-state", "data"),
     Input(component_id="my-input", component_property="value"),
