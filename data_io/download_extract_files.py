4b53b791755c5eaf2cf1f62b521f941f39cf6ea8
Tilo Himmelsbach <tilo.himmelsbach@iais.fraunhofer.de>
2022-03-21 09:28:20 +0100

    download_data

--- data_io/download_extract_files.py
+++ data_io/download_extract_files.py
@@ -2,15 +2,18 @@ import re
 
 import os
 
+from beartype import beartype
 
+
+@beartype
 def download_data(
-    base_url,
-    file_name,
-    data_folder,
-    verbose=False,
-    unzip_it=False,
-    do_raise=True,
-    remove_zipped=False,
+    base_url: str,
+    file_name: str,
+    data_folder: str,
+    verbose: bool = False,
+    unzip_it: bool = False,
+    do_raise: bool = True,
+    remove_zipped: bool = False,
 ):
     if not os.path.exists(data_folder):
         os.makedirs(data_folder, exist_ok=True)
@@ -23,9 +26,10 @@ def download_data(
 
     try:
         if unzip_it:
-            suffixes = [".zip", ".ZIP", ".tar.gz", ".tgz", ".gz", ".GZ"]
+            suffixes = [".zip", ".ZIP", ".tar.gz", ".tgz", ".gz", ".GZ", ".tar", ".TAR"]
             regex = r"|".join([f"(?:{s})" for s in suffixes])
             extract_folder = re.sub(regex, "", file)
+            assert extract_folder != file
 
             if any(file.endswith(suf) for suf in [".zip", ".ZIP"]):
 
@@ -37,6 +41,11 @@ def download_data(
                 def build_command(dirr, file):
                     return f"tar xzf {file} -C {dirr}"
 
+            elif any(file.endswith(suf) for suf in [".tar", ".TAR"]):
+
+                def build_command(dirr, file):
+                    return f"tar xf {file} -C {dirr}"
+
             elif any(file.endswith(suf) for suf in [".gz", ".GZ"]):
 
                 def build_command(dirr, file):
@@ -47,7 +56,7 @@ def download_data(
 
             if not os.path.isdir(extract_folder):
                 wget_file(url, data_folder, verbose)
-                assert os.system(f"mkdir {extract_folder}") == 0
+                os.makedirs(extract_folder, exist_ok=True)
                 extract(extract_folder, file, build_command)
                 if remove_zipped:
                     os.remove(file)
