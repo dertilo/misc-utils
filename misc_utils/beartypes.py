77f4fb6aba315953c3c8daa91f11f0cb621819da
Tilo Himmelsbach <tilo.himmelsbach@iais.fraunhofer.de>
2022-04-20 13:40:15 +0200

    numpy optional

--- misc_utils/beartypes.py
+++ misc_utils/beartypes.py
@@ -4,24 +4,30 @@ from typing import Annotated, TypeVar, Callable
 import beartype
 from beartype.roar import BeartypeCallException
 from beartype.vale import IsAttr, IsEqual, Is
-from numpy import floating, int16, number
-from numpy.typing import NDArray
 
-NumpyArray = NDArray[number]
-NumpyFloat2DArray = Annotated[NDArray[floating], IsAttr["ndim", IsEqual[2]]]
-NeNumpyFloat2DArray = Annotated[
-    NDArray[floating],
-    IsAttr["ndim", IsEqual[2]]
-    & Is[lambda x: x.shape[0] > 0]
-    & Is[lambda x: x.shape[1] > 0],
-]
-NeNumpyFloat1DArray = Annotated[
-    NDArray[floating], IsAttr["ndim", IsEqual[1]] & Is[lambda x: x.shape[0] > 0]
-]
-NumpyFloat1DArray = Annotated[NDArray[floating], IsAttr["ndim", IsEqual[1]]]
-# TODO: rename to NumpyFloatDim1, NumpyFloat32Dim1, etc
-Numpy1DArray = Annotated[NDArray[number], IsAttr["ndim", IsEqual[1]]]
-NumpyInt16Dim1 = Annotated[NDArray[int16], IsAttr["ndim", IsEqual[1]]]
+try:
+    # TODO: looks somewhat ugly!
+    from numpy import floating, int16, number
+    from numpy.typing import NDArray
+
+    NumpyArray = NDArray[number]
+    NumpyFloat2DArray = Annotated[NDArray[floating], IsAttr["ndim", IsEqual[2]]]
+    NeNumpyFloat2DArray = Annotated[
+        NDArray[floating],
+        IsAttr["ndim", IsEqual[2]]
+        & Is[lambda x: x.shape[0] > 0]
+        & Is[lambda x: x.shape[1] > 0],
+    ]
+    NeNumpyFloat1DArray = Annotated[
+        NDArray[floating], IsAttr["ndim", IsEqual[1]] & Is[lambda x: x.shape[0] > 0]
+    ]
+    NumpyFloat1DArray = Annotated[NDArray[floating], IsAttr["ndim", IsEqual[1]]]
+    # TODO: rename to NumpyFloatDim1, NumpyFloat32Dim1, etc
+    Numpy1DArray = Annotated[NDArray[number], IsAttr["ndim", IsEqual[1]]]
+    NumpyInt16Dim1 = Annotated[NDArray[int16], IsAttr["ndim", IsEqual[1]]]
+
+except ImportError:
+    print("no numpy installed!")
 
 NeStr = Annotated[str, Is[lambda s: len(s) > 0]]
 Dataclass = Annotated[object, Is[lambda o: dataclasses.is_dataclass(o)]]
--- tests/test_beartyped_dataclass.py
+++ tests/test_beartyped_dataclass.py
@@ -2,19 +2,27 @@ import os
 from dataclasses import field, dataclass
 from typing import Annotated, List, Optional
 
-import numpy as np
 import pytest
 from beartype import beartype
 from beartype.roar import BeartypeCallHintPepParamException
 from beartype.vale import IsAttr, IsEqual, Is
-from numpy import floating
-from numpy.typing import NDArray
 
 from misc_utils.beartypes import bear_does_roar
 
 assert os.environ.get("BEARTYPE_DATACLASSES_BASEDIR", "") != ""
+numpy_is_installed = False
+try:
+    import numpy as np
+    from numpy import floating
+    from numpy.typing import NDArray
 
-Numpy2DArray = Annotated[NDArray[floating], IsAttr["ndim", IsEqual[2]]]
+    Numpy2DArray = Annotated[NDArray[floating], IsAttr["ndim", IsEqual[2]]]
+
+    numpy_is_installed = True
+
+except ImportError:
+    Numpy2DArray = None
+    print("no numpy installed!")
 
 LengthyString = Annotated[str, Is[lambda text: 4 <= len(text) <= 40]]
 
@@ -33,7 +41,7 @@ except:
 
 
 @dataclass
-class WrappedNumpy2DArray:
+class WantsNumpy2DArray:
     data: Numpy2DArray = field(compare=False, repr=False)
 
     @beartype
@@ -42,19 +50,25 @@ class WrappedNumpy2DArray:
 
 
 @dataclass
-class DataContainer(WrappedNumpy2DArray):
+class WantsALenghlyString:
     lenghtly_string: LengthyString
 
 
-def test_beartyped_field():
+@pytest.mark.skipif(not numpy_is_installed, reason="numpy is not installed")
+def test_WantsALenghlyString():
+
+    _ = WantsNumpy2DArray(data=np.zeros((6, 3)))
+
+    assert bear_does_roar(lambda: WantsNumpy2DArray(data=np.zeros((6, 3))))
+
+
+def test_WantsALenghlyString():
     valid_string = "x" * 5
     invalid_string = "x" * 3
 
-    _ = DataContainer(data=np.zeros((6, 3)), lenghtly_string=valid_string)
+    _ = WantsALenghlyString(lenghtly_string=valid_string)
 
-    assert bear_does_roar(
-        lambda: DataContainer(data=np.zeros((6, 3)), lenghtly_string=invalid_string)
-    )
+    assert bear_does_roar(lambda: WantsALenghlyString(lenghtly_string=invalid_string))
 
 
 @dataclass
