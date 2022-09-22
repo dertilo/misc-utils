cd4466f5532b2859f80efb0a18a7f47da5abc3b9
Tilo Himmelsbach <dertilo@gmail.com>
2022-09-22 10:42:47 +0200

    enforce python version

--- misc_utils/__init__.py
+++ misc_utils/__init__.py
@@ -3,3 +3,8 @@ from misc_utils.beartyped_dataclass_patch import (
 )
 
 beartype_all_dataclasses_of_this_files_parent(__file__)
+
+import sys
+
+if not (sys.version_info.major == 3 and sys.version_info.minor == 9):
+    raise Exception("Python 3.9 is required.")
